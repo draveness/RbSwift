52a68b8e206c4c6b1e823a2b828d7424ccbadc1f
draveness <i@draveness.me>
2017-03-19 20:19:45 +0800

    Add to_f and to_double var to str

--- RbSwiftTests/String/String+ConversionsSpec.swift
+++ RbSwiftTests/String/String+ConversionsSpec.swift
@@ -14,21 +14,49 @@ class StringConversionsSpec: QuickSpec {
     override func spec() {
         describe(".to_i") {
             it("returns an integer under different circumstances") {
-                expect("12".to_i).to(equal(12))
-                expect("0a".to_i).to(equal(0))
                 expect("0a".to_i(16)).to(equal(10))
-                expect("0a".to_i(37)).to(equal(0))
-                expect("0a".to_i(1)).to(equal(0))
-                expect("hello".to_i).to(equal(0))
+                expect("12".to_i).to(equal(12))
+                expect("-1100101".to_i(2)).to(equal(-101))
                 expect("1100101".to_i(2)).to(equal(101))
                 expect("1100101".to_i(8)).to(equal(294977))
                 expect("1100101".to_i(10)).to(equal(1100101))
                 expect("1100101".to_i(16)).to(equal(17826049))
             }
+            
+            it("returns zero if base is not in 2...36") {
+                expect("".to_i).to(equal(0))
+                expect("  ".to_i).to(equal(0))
+                expect("0a".to_i(1)).to(equal(0))
+                expect("0a".to_i(37)).to(equal(0))
+            }
+            
+            it("returns zero if format is invalid") {
+                expect("-".to_i).to(equal(0))
+                expect("d-1".to_i).to(equal(0))
+                expect("0a".to_i).to(equal(0))
+                expect("hello".to_i).to(equal(0))
+            }
         }
         
-        describe(".to_datetime") { 
-            it("returns a Date using different parsing techniques") {
+        describe(".to_double") {
+            it("returns an float under different circumstances") {
+                expect("0a".to_double).to(equal(0))
+                expect("1100101.11".to_double).to(equal(1100101.11))
+                expect("123.456".to_double).to(equal(123.456))
+                expect("123.456ddddd".to_double).to(equal(123.456))
+            }
+            
+            it("returns zero if format is invalid") {
+                expect("-".to_double).to(equal(0))
+                expect("d-1".to_double).to(equal(0))
+                expect("0a".to_double).to(equal(0))
+                expect("..12".to_double).to(equal(0))
+                expect("hello".to_double).to(equal(0))
+            }
+        }
+        
+        describe(".to_datetime") {
+            it("returns a Date with RFC2822 form") {
                 let date = "Sun Mar 19 01:04:21 2017".to_datetime!
                 expect(date.year).to(equal(2017))
                 expect(date.month).to(equal(3))
@@ -38,7 +66,7 @@ class StringConversionsSpec: QuickSpec {
                 expect(date.second).to(equal(21))
             }
             
-            it("returns a Date using different parsing techniques") {
+            it("returns a Date with custom form") {
                 let date = "2017-03-19 00:35:36 +0800".to_datetime!
                 expect(date.year).to(equal(2017))
                 expect(date.month).to(equal(3))
