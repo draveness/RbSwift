28e64ae72fcc0fabbb703fe77d8059b08d5233da
draveness <i@draveness.me>
2017-03-19 22:38:14 +0800

    Remove extra methods

--- RbSwift/String/String+Operator.swift
+++ RbSwift/String/String+Operator.swift
@@ -10,20 +10,49 @@ import Foundation
 
 infix operator =~
 
-public func =~(left: String, right: String) -> Bool {
-    let regex = try! NSRegularExpression(pattern: right, options: NSRegularExpression.Options(rawValue: 0))
-    guard let _ = regex.firstMatch(in: left, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, left.length)) else { return false }
+/// Returns true if right pattern is match with left string, the pattern string would converts to a NSRegularExpression
+/// automaticlly in method.
+///
+/// - Parameters:
+///   - str: A string waiting to match
+///   - pattern: A pattern used to match the string
+/// - Returns: A bool value indicates whether the str is matched with the pattern
+public func =~(str: String, pattern: String) -> Bool {
+    let regex = try! NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options(rawValue: 0))
+    guard let _ = regex.firstMatch(in: str, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, str.length)) else { return false }
     return true
 }
 
+/// Returns true if right pattern is match with left character, the pattern string would converts to a NSRegularExpression
+/// automaticlly in method. This method use =~(str:pattern) internal to match char with pattern.
+///
+/// - Parameters:
+///   - left: A `Character` waiting to match
+///   - right: A pattern used to match the string
+/// - Returns: A bool value indicates whether the char is matched with the pattern
 public func =~(left: Character, right: String) -> Bool {
     return String(left) =~ right
 }
 
+/// Returns true if right `Character` which converted to a `NSRegularExpression` is match with left character, 
+/// the pattern string would converts to a NSRegularExpression automaticlly in method. 
+/// This method use =~(str:pattern) internal to match char with pattern.
+///
+/// - Parameters:
+///   - str: A string waiting to match
+///   - right: A Character used to match the string, will convert to String, and eventually NSRegularExpression.
+/// - Returns: A bool value indicates whether the char is matched with the pattern
 public func =~(left: String, right: Character) -> Bool {
     return left =~ String(right)
 }
 
+/// Returns a new String containing integer copies of the receiver. integer must be greater than or equal to 0.
+/// If integer is less than of equal to 0, this methods will return an empty string.
+///
+/// - Parameters:
+///   - left: A string
+///   - right: A integer indicates how many times the string will duplicate
+/// - Returns: A new String containing integer copies of the receiver
 public func *(left: String, right: Int) -> String {
     guard right > 0 else { return "" }
     var result = ""
@@ -33,10 +62,23 @@ public func *(left: String, right: Int) -> String {
     return result
 }
 
+/// Returns a new String containing integer copies of the receiver. integer must be greater than or equal to 0.
+/// If integer is less than of equal to 0, this methods will return an empty string.
+///
+/// - Parameters:
+///   - left: A integer indicates how many times the string will duplicate
+///   - right: A string
+/// - Returns: A new String containing integer copies of the receiver
 public func *(left: Int, right: String) -> String {
     return right * left
 }
 
+/// Returns a new String containing the two strings passed into this operator.
+///
+/// - Parameters:
+///   - left: A string
+///   - right: Anther string
+/// - Returns: A new string containing the two strings passed into this operator
 public func <<(left: String, right: String) -> String {
     return left + right
 }
