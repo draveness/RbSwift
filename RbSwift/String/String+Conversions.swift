b4618b8b35663aa8d1a5fb9ef8b597894472f38c
draveness <i@draveness.me>
2017-03-19 20:19:45 +0800

    Add to_f and to_double var to str

--- RbSwift/String/String+Conversions.swift
+++ RbSwift/String/String+Conversions.swift
@@ -10,12 +10,14 @@ import Foundation
 
 fileprivate let radix = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+fileprivate let integers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 // MARK: - Conversions
 public extension String {
     /// Returns the result of interpreting leading characters in str as an integer base (between 2 and 36).
     /// Extraneous characters past the end of a valid number are ignored. 
     /// If there is not a valid number at the start of str, 0 is returned. 
-    /// This method returns 0 when base is valid.
+    /// This method returns 0 when base is invalid.
     var to_i: Int {
         return to_i(10)
     }
@@ -23,15 +25,21 @@ public extension String {
     /// Returns the result of interpreting leading characters in str as an integer base (between 2 and 36).
     /// Extraneous characters past the end of a valid number are ignored.
     /// If there is not a valid number at the start of str, 0 is returned.
-    /// This method returns 0 when base is valid.
+    /// This method returns 0 when base is invalid.
     ///
     /// - Parameter base: A int indicates the integer base
     /// - Returns: A integer based on the integer base
     func to_i(_ base: Int = 10) -> Int {
         guard Array(2...36).contains(base) else { return 0 }
         let nums = radix[0..<base]
-        var result = ""
-        for char in split("") {
+        var chars = split("")
+        
+        // deal with the sign
+        let sign = chars.first == "-" ? -1 : 1
+        if chars.first == "-" { chars.remove(at: 0) }
+
+        var result: String = ""
+        for char in chars {
             guard nums.contains(char) else { break }
             result.append(char)
         }
@@ -40,7 +48,53 @@ public extension String {
         for (index, bit) in result.reverse.split("").enumerated() {
             value += Int(pow(Double(base), Double(index))) * nums.index(of: bit)!
         }
-        return value
+        return value * sign
+    }
+    
+    /// An alias to `to_double` but returns `Float` instead.
+    var to_f: Float {
+        return to_double.to_f
+    }
+    
+    /// Returns the result of interpreting leading characters in str as a double
+    /// Extraneous characters past the end of a valid number are ignored.
+    /// If there is not a valid number at the start of str, 0.0 is returned.
+    var to_double: Double {
+        var chars = split("")
+        
+        // deal with the sign
+        let sign: Double = chars.first == "-" ? -1.0 : 1.0
+        if chars.first == "-" { chars.remove(at: 0) }
+        
+        var hasDot = false
+        var integer: String = ""
+        var decimal: String = ""
+        for char in chars {
+            if char == "." {
+                if hasDot {
+                    break
+                } else {
+                    hasDot = true
+                    continue
+                }
+            }
+            if integers.contains(char) {
+                if hasDot {
+                    decimal.append(char)
+                } else {
+                    integer.append(char)
+                }
+            } else {
+                break
+            }
+        }
+        
+        if decimal.length > 0 {
+            return (integer.to_i.to_double + decimal.to_i.to_double / pow(10.0, decimal.length.to_double)) * sign
+        } else {
+            return integer.to_i.to_double * sign
+        }
+        
     }
     
     /// Returns the result of interpreting the receiver in different standard.
