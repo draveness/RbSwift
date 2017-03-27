736cb05f2725da4cb9a0c25101d7a245433a8965
Draveness <i@draveness.me>
2017-03-27 15:35:33 +0800

    Add text.rb and update doc for bool

--- RbSwift/String/String+Bool.swift
+++ RbSwift/String/String+Bool.swift
@@ -12,6 +12,12 @@ import Foundation
 public extension String {
     /// Returns `true` if receiver contains the given strings or characters array.
     ///
+    /// 	"  ".isInclude("he")                    #=> false
+    /// 	"he".isInclude("he")                    #=> true
+    /// 	"ohe".isInclude("he")                   #=> true
+    /// 	"ohe".isInclude("he", "oooo")           #=> true
+    /// 	"oe".isInclude("he", "dajkldjda")       #=> false
+    ///
     /// - Parameter substrings: An array of strings or characters
     /// - Returns: A bool value indicates whether the string includes the other string
     func isInclude(_ substrings: String...) -> Bool {
@@ -19,6 +25,12 @@ public extension String {
     }
 
     /// Returns `true` if receiver contains the given strings or characters array.
+    ///
+    /// 	"  ".isInclude("he")                    #=> false
+    /// 	"he".isInclude("he")                    #=> true
+    /// 	"ohe".isInclude("he")                   #=> true
+    /// 	"ohe".isInclude("he", "oooo")           #=> true
+    /// 	"oe".isInclude("he", "dajkldjda")       #=> false
     ///
     /// - Parameter substrings: An array of strings or characters
     /// - Returns: A bool value indicates whether the string includes the other string
@@ -28,6 +40,12 @@ public extension String {
     
     /// Returns `true` if receiver does not contains the given strings or characters array.
     ///
+    /// 	"  ".isExlude("he")                     #=> true
+    /// 	"he".isExlude("he")                     #=> false
+    /// 	"ohe".isExlude("he")                    #=> false
+    /// 	"ohe".isExlude("he", "oooo")            #=> false
+    /// 	"oe".isExlude("he", "dajkldjda")		#=> true
+    ///
     /// - Parameter substrings: An array of strings or characters
     /// - Returns: A bool value indicates whether the string excludes the other string
     func isExlude(_ substrings: String...) -> Bool {
@@ -35,6 +53,12 @@ public extension String {
     }
     
     /// Returns `true` if receiver does not contains the given strings or characters array.
+    ///
+    /// 	"  ".isExlude("he")                     #=> true
+    /// 	"he".isExlude("he")                     #=> false
+    /// 	"ohe".isExlude("he")                    #=> false
+    /// 	"ohe".isExlude("he", "oooo")            #=> false
+    /// 	"oe".isExlude("he", "dajkldjda")		#=> true
     ///
     /// - Parameter substrings: An array of strings or characters
     /// - Returns: A bool value indicates whether the string excludes the other string
@@ -44,6 +68,10 @@ public extension String {
     
     /// Returns true if str starts with one of the prefixes given.
     ///
+    /// 	"he".isStartWith("he")                  #=> true
+    /// 	"ohe".isStartWith("he", "oooo")         #=> false
+    /// 	"oe".isStartWith("o", "dajkldjda")		#=> true
+    ///
     /// - Parameter substrings: An array of prefixes
     /// - Returns: A bool value indicates whether the string starts with another string in array
     func isStartWith(_ substrings: String...) -> Bool {
@@ -51,6 +79,10 @@ public extension String {
     }
 
     /// Returns true if str starts with one of the prefixes given.
+    ///
+    /// 	"he".isStartWith("he")                  #=> true
+    /// 	"ohe".isStartWith("he", "oooo")         #=> false
+    /// 	"oe".isStartWith("o", "dajkldjda")		#=> true
     ///
     /// - Parameter substrings: An array of prefixes
     /// - Returns: A bool value indicates whether the string starts with another string in array
@@ -60,6 +92,12 @@ public extension String {
     
     /// Returns true if str ends with one of the suffixes given.
     ///
+    /// 	"  ".isEndWith("he")                    #=> false
+    /// 	"he".isEndWith("he")                    #=> true
+    /// 	"ohe".isEndWith("he")                   #=> true
+    /// 	"ohe".isEndWith("he", "oooo")           #=> true
+    /// 	"oe".isEndWith("he", "dajkldjda")		#=> false
+    ///
     /// - Parameter substrings: An array of suffixes
     /// - Returns: A bool value indicates whether the string ends with another string in array
     func isEndWith(_ substrings: String...) -> Bool {
@@ -68,15 +106,30 @@ public extension String {
     
     /// Returns true if str ends with one of the suffixes given.
     ///
+    /// 	"  ".isEndWith("he")                    #=> false
+    /// 	"he".isEndWith("he")                    #=> true
+    /// 	"ohe".isEndWith("he")                   #=> true
+    /// 	"ohe".isEndWith("he", "oooo")           #=> true
+    /// 	"oe".isEndWith("he", "dajkldjda")		#=> false
+    ///
     /// - Parameter substrings: An array of suffixes
     /// - Returns: A bool value indicates whether the string ends with another string in array
     func isEndWith(_ substrings: [String]) -> Bool {
         return substrings.flatMap(hasSuffix).isAny { $0 == true }
     }
     
-    /// Converts pattern to a `NSReguarExpression`, then returns a true or false indicates whether 
+    /// Converts pattern to a `NSReguarExpression`, then returns a bool value indicates whether
     /// the `NSReguarExpression` is matched receiver or not. 
+    ///
+    /// 	"  ".isMatch("he")              #=> false
+    /// 	"he".isMatch("he")              #=> true
+    /// 	"oe".isMatch("he")              #=> false
+    /// 	"oe".isMatch(".e")              #=> true
+    /// 	"abcdefghijklmnopqrstuvwxyz".isMatch(".*")		#=> true
+    ///
     /// If the second parameter is present, it specifies the position in the string to begin the search.
+    ///
+    /// 	"ohe".isMatch("he", 2)          #=> false
     ///
     /// - Parameters:
     ///   - pattern: A string pattern
@@ -89,16 +142,30 @@ public extension String {
     }
 
     /// Returns true is the recevier string's characters are all whitespaces, like `\r`, `\n`, `\t` and ` `.
+    ///
+    /// 	"  ".isBlank            #=> true
+    /// 	"\t\n\r\n".isBlank		#=> true
+    /// 	"\t\nblah".isBlank		#=> false
+    ///
     var isBlank: Bool {
         return self.chars.reduce(true) { $0 && ($1 =~ "[\r\n\t ]".to_regex) }
     }
     
     /// Returns true is the recevier string's characters are all downcase.
+    ///
+    /// 	"HELLO".isDowncase		#=> false
+    /// 	"HELLOo".isDowncase		#=> false
+    /// 	"hello".isDowncase		#=> true
+    ///
     var isDowncase: Bool {
         return self.downcase == self
     }
     
     /// Returns true is the recevier string's characters are all upcase.
+    ///
+    /// 	"HELLO".isUpcase		#=> true
+    /// 	"HELLOo".isUpcase		#=> false
+    ///
     var isUpcase: Bool {
         return self.upcase == self
     }
---
+++ text.rb
@@ -0,0 +1,11 @@
+content = File.read "text"
+content = content.each_line.map do |line|
+    line.gsub("expect(", "/// \t")
+        .gsub(").to(equal(", "\t\t#=> ")
+        .gsub(").to(beNil(", "\t\t#=> nil")
+        .gsub(").to(beTrue(", "\t\t#=> true")
+        .gsub(").to(beFalse(", "\t\t#=> false")
+        .gsub(/\)\)$/, "")
+end
+
+puts content.join
