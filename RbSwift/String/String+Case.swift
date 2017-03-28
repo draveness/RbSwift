01f89c6a7e9b77bc6cc9ffa8fe4e2752ec77a969
Draveness <i@draveness.me>
2017-03-28 11:34:16 +0800

    Complete string case docs

--- RbSwift/String/String+Case.swift
+++ RbSwift/String/String+Case.swift
@@ -10,12 +10,20 @@ import Foundation
 
 // MARK: - Case
 public extension String {
-    /// Returns a copy of str with all uppercase letters replaced with their lowercase counterparts. 
+    /// Returns a copy of str with all uppercase letters replaced with their lowercase counterparts.
+    ///
+    /// 	"Hello".downcase		#=> "hello"
+    /// 	"HellHEo".downcase		#=> "hellheo"    
+    ///
     var downcase: String {
         return self.lowercased()
     }
     
     /// Downcases the contents of the receiver
+    ///
+    ///     var hello = "Hello"
+    ///     hello.downcased()       #=> "hello"
+    /// 	hello                   #=> "hello"
     ///
     /// - Returns: Self
     @discardableResult mutating func downcased() -> String {
@@ -24,11 +32,19 @@ public extension String {
     }
     
     /// Returns a copy of str with all lowercase letters replaced with their uppercase counterparts.
+    ///
+    /// 	"Hello".upcase          #=> "HELLO"
+    /// 	"HellHEo".upcase		#=> "HELLHEO"
+    ///
     var upcase: String {
         return self.uppercased()
     }
     
     /// Upcases the contents of the receiver
+    ///
+    ///     var hello = "Hello"
+    ///     hello.upcased()         #=> "HELLO"
+    /// 	hello                   #=> "HELLO"
     ///
     /// - Returns: Self
     @discardableResult mutating func upcased() -> String {
@@ -37,11 +53,18 @@ public extension String {
     }
     
     /// Returns a copy of str with uppercase alphabetic characters converted to lowercase and lowercase characters converted to uppercase.
+    ///
+    /// 	"HellHEo".swapcase		#=> "hELLheO"
+    ///
     var swapcase: String {
         return self.chars.map { $0.isUpcase ? $0.downcase : $0.upcase }.joined()
     }
     
     /// Equivalent to `swapcase`, but modifies the receiver in place.
+    ///
+    ///     let hello = "HellHEo"
+    /// 	hello.swapcased()       #=> "hELLheO"
+    /// 	hello                   #=> "hELLheO"
     ///
     /// - Returns: Self
     @discardableResult mutating func swapcased() -> String {
