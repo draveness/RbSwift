6ff8408171f7798988129cdaca47f8cc7e582201
Draveness <i@draveness.me>
2017-03-28 12:26:16 +0800

    Use lines in eachLine

--- RbSwift/String/String+Enumeration.swift
+++ RbSwift/String/String+Enumeration.swift
@@ -19,7 +19,7 @@ public extension String {
     ///   - separator: A string used to separate the receiver string
     ///   - closure: A closure reveives String as parameter which is separated by the separator
     func eachLine(_ separator: String = "\n", closure: (String) -> Void) {
-        var lines = components(separatedBy: separator)
+        var lines = self.lines(separator)
         for (index, line) in lines.enumerated() {
             if index != lines.count - 1 {
                 lines[index] = line + separator
