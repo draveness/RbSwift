3f2540336ff09efa82e538acf2ea8f9522da883f
draveness <i@draveness.me>
2017-03-30 22:38:39 +0800

    update rbswift

--- RbSwift/Date/Date+Time.swift
+++ RbSwift/Date/Date+Time.swift
@@ -25,14 +25,34 @@ public extension Date {
         return Date() - 1.day
     }
     
+    /// Returns a date with timestamp, use `Date(timeIntervalSince1970:)`
+    /// initializer to create a new date.
+    ///
+    /// - Parameter timestamp: A unix timestamp
+    /// - Returns: A date
     static func at(_ timestamp: Int) -> Date {
         return Date(timeIntervalSince1970: timestamp.to_double)
     }
     
+    /// Returns a date with timestamp, use `Date(timeIntervalSince1970:)`
+    /// initializer to create a new date.
+    ///
+    /// - Parameter timestamp: A unix timestamp
+    /// - Returns: A date
     static func at(_ timestamp: Double) -> Date {
         return Date(timeIntervalSince1970: timestamp)
     }
     
+    /// Returns a new Date where one or more of the elements have been changed according to the passing parameter. 
+    ///
+    /// - Parameters:
+    ///   - year: A int value
+    ///   - month: A int value
+    ///   - day: A int value
+    ///   - hour: A int value
+    ///   - minute: A int value
+    ///   - second: A int value
+    /// - Returns: A new date
     func change(year: Int? = nil, month: Int? = nil, day: Int? = nil,
                 hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
         var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: self)
@@ -45,10 +65,44 @@ public extension Date {
         return Calendar.current.date(from: dateComponents)!
     }
     
+    /// Returns and changes `self` where one or more of the elements have been changed according to the passing parameter.
+    ///
+    /// - Parameters:
+    ///   - year: A int value
+    ///   - month: A int value
+    ///   - day: A int value
+    ///   - hour: A int value
+    ///   - minute: A int value
+    ///   - second: A int value
+    /// - Returns: Self
     @discardableResult mutating func changed(year: Int? = nil, month: Int? = nil, day: Int? = nil,
                                     hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
         self = change(year: year, month: month, day: day, hour: hour, minute: minute, second: second)
         return self
+    }
+    
+    enum Period {
+        case midnight
+        case beginningOfDay
+        case noon
+        case midday
+        case middleOfDay
+        case endOfDay
+    }
+    
+    /// Adjusts receiver and returns an new date with corresponding time period.
+    ///
+    /// - Parameter period: An enum specify time period
+    /// - Returns: A new date
+    func at(_ period: Period) -> Date {
+        switch period {
+        case .midnight, .beginningOfDay:
+            return beginningOfDay
+        case .midday, .noon, .middleOfDay:
+            return middleOfDay
+        case .endOfDay:
+            return endOfDay
+        }
     }
     
     var beginningOfDay: Date {
@@ -73,26 +127,6 @@ public extension Date {
     
     var endOfDay: Date {
         return change(hour: 23, minute: 59, second: 59)
-    }
-    
-    enum Period {
-        case midnight
-        case beginningOfDay
-        case noon
-        case midday
-        case middleOfDay
-        case endOfDay
-    }
-    
-    func at(_ period: Period) -> Date {
-        switch period {
-        case .midnight, .beginningOfDay:
-            return beginningOfDay
-        case .midday, .noon, .middleOfDay:
-            return middleOfDay
-        case .endOfDay:
-            return endOfDay
-        }
     }
     
     var next: Date {
