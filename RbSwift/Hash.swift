d54b940397e129f4083706050fb60f123ba4ce9e
draveness <i@draveness.me>
2017-04-06 00:05:21 +0800

    Add hash methods and udpate matchdata and regex

--- RbSwift/Hash.swift
+++ RbSwift/Hash.swift
@@ -10,3 +10,15 @@ import Foundation
 
 /// Hash is an alias to Dictionary
 typealias Hash<K: Hashable, V> = Dictionary<K, V>
+
+public func + <K,V>(left: Dictionary<K,V>, right: Dictionary<K,V>)
+    -> Dictionary<K,V> {
+    var map = Dictionary<K,V>()
+    for (k, v) in left {
+        map[k] = v
+    }
+    for (k, v) in right {
+        map[k] = v
+    }
+    return map
+}
