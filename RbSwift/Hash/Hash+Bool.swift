d6e34deef1a540c8aa56d1ff1d6b880d65079875
draveness <i@draveness.me>
2017-04-06 21:47:23 +0800

    Add isAny(closure:) for Hash methods

--- RbSwift/Hash/Hash+Bool.swift
+++ RbSwift/Hash/Hash+Bool.swift
@@ -10,5 +10,9 @@ import Foundation
 
 // MARK: - Bool
 public extension Hash {
-    
+    func isAny(closure: (Key, Value) -> Bool) -> Bool {
+        return reduce(false) { (result, element) in
+            return result || closure(element.key, element.value)
+        }
+    }
 }
