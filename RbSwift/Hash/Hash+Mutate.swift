ab0ad28a355e589e0ea10da4204b943ab612bc06
draveness <i@draveness.me>
2017-04-07 00:03:54 +0800

    Add delete methods for hash

--- RbSwift/Hash/Hash+Mutate.swift
+++ RbSwift/Hash/Hash+Mutate.swift
@@ -10,28 +10,4 @@ import Foundation
 
 // MARK: - Mutate
 public extension Hash {
-    /// A mutating version of `Hash#merge(otherHash:closure:)`
-    ///
-    ///     let h1 = ["a": 100, "b": 200]
-    ///     let h2 = ["b": 254, "c": 300]
-    ///
-    /// 	h1.merge(h2)		#=> ["a": 100, "b": 254, "c": 300]))
-    /// 	h1                  #=> ["a": 100, "b": 254, "c": 300]))
-    ///
-    /// - Parameters:
-    ///   - otherHash: Another hash instance.
-    ///   - closure: A closure returns a new value if duplicate happens.
-    /// - Returns: Self
-    @discardableResult mutating func merged(_ otherHash: Hash<Key, Value>, closure: ((Key, Value, Value) -> Value)? = nil) -> Hash<Key, Value> {
-        self = self.merge(otherHash, closure: closure)
-        return self
-    }
-    
-    /// Removes all key-value pairs from hsh.
-    ///
-    /// - Returns: Self with empty hash.
-    @discardableResult mutating func clear() -> Hash<Key, Value> {
-        self = [:]
-        return self
-    }
 }
