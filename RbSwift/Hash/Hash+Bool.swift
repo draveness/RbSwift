6913c7a76abf22706b246a8ee326b4f43558dd03
draveness <i@draveness.me>
2017-04-06 21:50:57 +0800

    Remove pod install and add doc

--- RbSwift/Hash/Hash+Bool.swift
+++ RbSwift/Hash/Hash+Bool.swift
@@ -10,6 +10,15 @@ import Foundation
 
 // MARK: - Bool
 public extension Hash {
+    /// Passes each element of the collection to the given block. The method returns 
+    /// true if the block ever returns a value other than false or nil.
+    ///
+    ///     let h1 = ["a": 100, "b": 200]
+    /// 	h1.isAny { (key, _) in key == "a" }		#=> true
+    /// 	h1.isAny { (key, _) in key == "c" }		#=> false
+    ///
+    /// - Parameter closure: A closure accepts key and value pair from the hash.
+    /// - Returns: A bool value.
     func isAny(closure: (Key, Value) -> Bool) -> Bool {
         return reduce(false) { (result, element) in
             return result || closure(element.key, element.value)
