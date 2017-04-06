9f3ec22cd6f54a58271bce026641b6e2e585712b
draveness <i@draveness.me>
2017-04-06 21:17:52 +0800

    Add .build to gitignore and merge methods

---
+++ RbSwiftTests/Hash+TransformSpec.swift
@@ -0,0 +1,30 @@
+//
+//  Hash+TransformSpec.swift
+//  RbSwift
+//
+//  Created by draveness on 06/04/2017.
+//  Copyright © 2017 draveness. All rights reserved.
+//
+
+import Quick
+import Nimble
+import RbSwift
+
+class HashTransformSpec: QuickSpec {
+    override func spec() {
+        describe(".merge(otherHash:)") {
+            it("Returns a new hash containing the contents of otherHash and the contents of receiver") {
+                let h1 = ["a": 100, "b": 200]
+                let h2 = ["b": 254, "c": 300]
+                expect(h1.merge(h2)).to(equal(["a": 100, "b": 254, "c": 300]))                
+                expect(h1.merge(h2) { (key, oldval, newval) in
+                    newval - oldval
+                }).to(equal(["a": 100, "b": 54,  "c": 300]))
+                expect(h1).to(equal(["a": 100, "b": 200]))
+
+                expect(TimeZone.utc).to(equal(TimeZone(abbreviation: "UTC")!))
+                expect(TimeZone.gmt).to(equal(TimeZone(abbreviation: "GMT")!))
+            }
+        }
+    }
+}
