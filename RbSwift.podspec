23d8568d9ae6ef12b6c5d2b339d2918e3ce60cf9
Draveness <i@draveness.me>
2017-04-06 14:18:47 +0800

    Update code style

--- RbSwift.podspec
+++ RbSwift.podspec
@@ -7,7 +7,7 @@
 
 Pod::Spec.new do |s|
   s.name         = "RbSwift"
-  s.version      = "0.1.4"
+  s.version      = "0.1.5"
   s.summary      = "A Swift port of convenient methods in Ruby and ActiveSupport"
   s.description  = <<-DESC
   RbSwift introduces a series of methods which is inspired by Ruby core library and
--- RbSwift/Hash.swift
+++ RbSwift/Hash.swift
@@ -11,9 +11,9 @@ import Foundation
 /// Hash is an alias to Dictionary
 typealias Hash<K: Hashable, V> = Dictionary<K, V>
 
-public func + <K,V>(left: Dictionary<K,V>, right: Dictionary<K,V>)
-    -> Dictionary<K,V> {
-    var map = Dictionary<K,V>()
+public func + <K,V>(left: Dictionary<K, V>, right: Dictionary<K, V>)
+    -> Dictionary<K, V> {
+    var map = Dictionary<K, V>()
     for (k, v) in left {
         map[k] = v
     }
