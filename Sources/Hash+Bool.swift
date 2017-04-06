45bc30e9276bf39e18ae25f345a5f8bca67ce382
draveness <i@draveness.me>
2017-04-06 21:42:53 +0800

    Add macOS test back to project

---
+++ Sources/Hash+Bool.swift
@@ -0,0 +1,14 @@
+//
+//  Hash+Bool.swift
+//  RbSwift
+//
+//  Created by draveness on 06/04/2017.
+//  Copyright © 2017 draveness. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Bool
+public extension Hash {
+    
+}
