91854066bd2acd5c9e45e95cff6ff6a376cc35f1
Draveness <i@draveness.me>
2017-04-07 16:42:49 +0800

    Add assoc and rassoc methods and docs

--- RbSwift/Hash/Hash+Access.swift
+++ RbSwift/Hash/Hash+Access.swift
@@ -33,4 +33,39 @@ public extension Hash {
     func values(at keys: [Key]) -> [Value?] {
         return keys.map { self[$0] }
     }
+    
+    /// Searches through the hash with key using `subscript`. Returns the key-value tuple
+    /// or nil if no match is found.
+    ///
+    ///     let hash = [1: "one", 2: "two", 3: "three"]
+    /// 	hash.assoc(1)       #=> (1, "one")
+    /// 	hash.assoc(42)      #=> nil
+    ///
+    /// - Parameter key: An key of type `Key`
+    /// - Returns: A key-value tuple or `nil`.
+    func assoc(_ key: Key) -> (Key, Value)? {
+        guard let value = self[key] else { return nil }
+        return (key, value)
+    }
+}
+
+// MARK: - Access
+public extension Hash where Value: Equatable {
+    /// Searches through the hash with value using `subscript`. Returns the key-value tuple
+    /// or nil if no match is found.
+    ///
+    ///     let hash = [1: "one", 2: "two", 3: "three"]
+    /// 	hash.rassoc("one")          #=> (1, "one")
+    /// 	hash.rassoc("forty-two")    #=> nil
+    ///
+    /// - Parameter key: An key of type `Key`
+    /// - Returns: A key-value tuple or `nil`.
+    func rassoc(_ value: Value) -> (Key, Value)? {
+        for (k, v) in self {
+            if value == v {
+                return (k, v)
+            }
+        }
+        return nil
+    }
 }
