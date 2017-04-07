ac28a530d9de8aa6544d24ee48ff7d8bfd54b661
draveness <i@draveness.me>
2017-04-07 09:42:08 +0800

    update enumeration functions to return self after executing the closureg

---
+++ RbSwift/Hash/Hash+Enumeration.swift
@@ -0,0 +1,15 @@
+//
+//  Hash+Enumeration.swift
+//  RbSwift
+//
+//  Created by draveness on 07/04/2017.
+//  Copyright © 2017 draveness. All rights reserved.
+//
+
+import Foundation
+
+extension Hash {
+    func each(closure: (Key, Value) -> Void) {
+        
+    }
+}
