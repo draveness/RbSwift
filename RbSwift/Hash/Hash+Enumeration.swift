8dc4accf681c408886815f624be126dbb18480ce
draveness <i@draveness.me>
2017-04-07 09:57:11 +0800

    Add more enumeration methods to hash

--- RbSwift/Hash/Hash+Enumeration.swift
+++ RbSwift/Hash/Hash+Enumeration.swift
@@ -8,8 +8,56 @@
 
 import Foundation
 
-extension Hash {
-    func each(closure: (Key, Value) -> Void) {
-        
+// MARK: - Enumeration
+public extension Hash {
+    /// Calls block once for each key in hsh, passing the key-value pair as parameters.
+    /// An alias to `Sequence#each(closure:)` method.
+    ///
+    ///     let hash = ["a": 100, "b": 200]
+    ///     var result: [String: Int] = [:]
+    ///     hash.eachPair {
+    ///         result[$0] = $1 + 100
+    ///     })          #=> ["a": 100, "b": 200]
+    ///
+    ///     result      #=> ["a": 200, "b": 300]
+    ///
+    /// - Parameter closure: An closure accepts key-value pair as parameters.
+    /// - Returns: Self.
+    @discardableResult func eachPair(closure: (Key, Value) -> Void) -> Hash<Key, Value> {
+        return self.each(closure)
+    }
+    
+    /// Calls block once for each key in hsh, passing the key as parameters.
+    ///
+    ///     let hash = ["a": 100, "b": 200]
+    ///     var result: [String] = []
+    ///     hash.eachKey {
+    ///         result.append($0)
+    ///     })          #=> ["a", "b"]
+    ///
+    ///     result      #=> ["a": 100, "b": 200]
+    ///
+    /// - Parameter closure: An closure accepts key as parameters.
+    /// - Returns: Self.
+    @discardableResult func eachKey(closure: (Key) -> Void) -> Hash<Key, Value> {
+        self.keys.reverseEach(closure)
+        return self
+    }
+    
+    /// Calls block once for each key in hsh, passing the value as parameters.
+    ///
+    ///     let hash = ["a": 100, "b": 200]
+    ///     var result: [Int] = []
+    ///     hash.eachValue {
+    ///         result.append($0)
+    ///     })          #=> [100, 200]
+    ///
+    ///     result      #=> ["a": 100, "b": 200]
+    ///
+    /// - Parameter closure: An closure accepts value as parameters.
+    /// - Returns: Self.
+    @discardableResult func eachValue(closure: (Value) -> Void) -> Hash<Key, Value> {
+        self.values.reverseEach(closure)
+        return self
     }
 }
--- RbSwiftTests/Hash/Hash+EnumerationSpec.swift
+++ RbSwiftTests/Hash/Hash+EnumerationSpec.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2017 draveness. All rights reserved.
 //
 
-import Foundation
+import Quick
+import Nimble
+import RbSwift
+
+class HashEnumerationSpec: QuickSpec {
+    override func spec() {
+        describe(".eachPair(closure:)") {
+            it("calls block once for each key in hsh, passing the key-value pair as parameters.") {
+                let hash = ["a": 100, "b": 200]
+                var result: [String: Int] = [:]
+                expect(hash.eachPair {
+                    result[$0] = $1 + 100
+                }).to(equal(hash))
+                expect(result).to(equal(["a": 200, "b": 300]))
+            }
+        }
+        
+        describe(".eachKey(closure:)") {
+            it("calls block once for each key in hsh, passing the key as parameters.") {
+                let hash = ["a": 100, "b": 200]
+                var result: [String] = []
+                expect(hash.eachKey {
+                    result.append($0)
+                }).to(equal(hash))
+                expect(result).to(equal(["a", "b"]))
+            }
+        }
+        
+        describe(".eachValue(closure:)") {
+            it("calls block once for each key in hsh, passing the value as parameters.") {
+                let hash = ["a": 100, "b": 200]
+                var result: [Int] = []
+                expect(hash.eachValue {
+                    result.append($0)
+                }).to(equal(hash))
+                expect(result).to(equal([100, 200]))
+            }
+        }
+    }
+}
