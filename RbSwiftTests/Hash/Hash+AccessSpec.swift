e5a26126c4754c94214c0fbe10937b9ea37eb5b6
Draveness <i@draveness.me>
2017-04-07 16:42:49 +0800

    Add assoc and rassoc methods and docs

--- RbSwiftTests/Hash/Hash+AccessSpec.swift
+++ RbSwiftTests/Hash/Hash+AccessSpec.swift
@@ -19,5 +19,23 @@ class HashAccessSpec: QuickSpec {
                 expect(hash.values(at: "catcat", "dog")).to(equal([nil, "canine"]))
             }
         }
+        
+        describe(".assoc(key:)") {
+            it("returns the first key-value pair (two-element array) that matches with given key.") {
+                let hash = [1: "one", 2: "two", 3: "three"]
+                expect(hash.assoc(1)!.0).to(equal(1))
+                expect(hash.assoc(1)!.1).to(equal("one"))
+                expect(hash.assoc(100)).to(beNil())
+            }
+        }
+        
+        describe(".rassoc(value:)") {
+            it("returns the first key-value pair (two-element array) that matches with given value.") {
+                let hash = [1: "one", 2: "two", 3: "three"]
+                expect(hash.rassoc("one")!.0).to(equal(1))
+                expect(hash.rassoc("one")!.1).to(equal("one"))
+                expect(hash.rassoc("forty-two")).to(beNil())
+            }
+        }
     }
 }
