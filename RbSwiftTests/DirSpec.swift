e360fdc4c4c197de54c4c391891f07b850ef6e90
draveness <i@draveness.me>
2017-04-10 19:51:25 +0800

    Add more methods to file extension

--- RbSwiftTests/DirSpec.swift
+++ RbSwiftTests/DirSpec.swift
@@ -14,7 +14,20 @@ class DirSpec: QuickSpec {
     override func spec() {
         describe(".home(path:)") {
             it("returns the home directory of the current user or the named user if given.") {
-                print(Dir.home())
+                expect(Dir.home("user")).to(equal("/user"))
+            }
+        }
+        
+        describe(".chdir(path:)") {
+            it("changes the curernt working directory of the process to the ginven string.") {
+                try! Dir.mkdir("draveness/spool/mail", recursive: true)
+                Dir.chdir("draveness/spool/mail")
+                expect(Dir.pwd.hasSuffix("draveness/spool/mail")).to(beTrue())
+                
+                let value = Dir.chdir("draveness") {
+                    return "Inside another directory"
+                }
+                expect(value).to(equal("Inside another directory"))
             }
         }
     }
