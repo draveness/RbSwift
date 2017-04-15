97739ca3d089e10fd0eabaf3e3806dfc5a0ac7ce
draveness <i@draveness.me>
2017-04-15 23:27:20 +0800

    Add array#to_sentence methods to conversion ext

--- RbSwiftTests/Array/Array+ConversionsSpec.swift
+++ RbSwiftTests/Array/Array+ConversionsSpec.swift
@@ -19,5 +19,16 @@ class ArrayConversionsSpec: QuickSpec {
                 expect([0,0,0,1].to_i(2)).to(equal(8))
             }
         }
+        
+        describe(".to_sentence(wordsConnector:twoWordsConnector:lastWordsConnector:)") {
+            it("converts the array to a comma-separated sentence where the last element is joined by the connector word") {
+                expect([].to_sentence).to(equal(""))
+                expect(["one"].to_sentence).to(equal("one"))
+                expect(["one", "two"].to_sentence).to(equal("one and two"))
+                expect(["one", "two", "three"].to_sentence).to(equal("one, two, and three"))
+                expect(["one", "two"].to_sentence(twoWordsConnector: "-")).to(equal("one-two"))
+                expect(["one", "two", "three"].to_sentence(wordsConnector: " or ", lastWordConnector: " or at least ")).to(equal("one or two or at least three"))
+            }
+        }
     }
 }
