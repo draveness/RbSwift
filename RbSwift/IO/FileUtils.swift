319710a4d4b5dd31c47933b26954975abc94161e
draveness <i@draveness.me>
2017-10-07 12:01:52 +0800

    feat: add FileUtils.cd and FileUtils.chdir methods

--- RbSwift/IO/FileUtils.swift
+++ RbSwift/IO/FileUtils.swift
@@ -9,6 +9,42 @@
 import Foundation
 
 public class FileUtils {
+    /// Changes the current directory to the directory dir.
+    ///
+    ///     FileUtils.cd('/', verbose: true)
+    ///
+    /// If this method is called with block, resumes to the old working directory after
+    /// the block execution finished.
+    ///
+    ///     FileUtils.cd('/') {
+    ///         // do somthing.
+    ///     }
+    ///
+    /// - Parameters:
+    ///   - dir: A directory path.
+    ///   - verbose: A bool value indicates print the child directory.
+    ///   - closure: A closure if provided would execute in new directory and resume the old directory
+    ///              after executing.
+    public static func cd(_ dir: String = "~", verbose: Bool = false, closure: (() -> ())? = nil) {
+        let originalPath = Dir.pwd
+        Dir.chdir(dir)
+        if verbose { print("Change workding directory to \(Dir.pwd))") }
+        guard let closure = closure else { return }
+        closure()
+        Dir.chdir(originalPath)
+    }
+
+    /// An alias to `FileUtils.cd(dir:verbose:closure:)`
+    ///
+    /// - Parameters:
+    ///   - dir: A directory path.
+    ///   - verbose: A bool value indicates print the child directory.
+    ///   - closure: A closure if provided would execute in new directory and resume the old directory
+    ///              after executing.
+    public static func chdir(_ dir: String = "~", verbose: Bool = false, closure: (() -> ())? = nil) {
+        return cd(dir, verbose: verbose, closure: closure)
+    }
+
     /// Removes directories and its all contents recursively.
     ///
     /// - Parameter paths: An array of file path.
